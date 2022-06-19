b0b2378d7d553786329377d61aaffa45270c3c14
itzikbs1 <92460446+itzikbs1@users.noreply.github.com>
2022-06-19 10:01:28 +0300

    Update Schedule.cpp

--- b-ball-main/Schedule.cpp
+++ b-ball-main/Schedule.cpp
@@ -1,29 +1,22 @@
 #include <iostream>
 #include "Schedule.hpp"
 
-
 using namespace std;
 
 namespace ariel{
 Schedule::Schedule(){
-
 }
        
 Schedule::Schedule(vector<Team*> teams){
     this->teams = teams;
 }
-
 vector<Game*> Schedule::Create(){
     int size = teams.size();
     for (size_t i = 0; i < size; i++)
     {
         for (size_t k = 0; k < size; k++)
         {
-            if(i==k){
-                continue;
-            }else{
-                this->scheduale.push_back(new Game(this->teams.at(i), this->teams.at(k)));
-            }
+            this->scheduale.push_back(new Game(this->teams.at(i), this->teams.at(k)));
         }
     }
     return this->scheduale;
